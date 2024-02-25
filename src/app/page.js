5761a9779dc074d3ad75d6e7891789c48af8d2fd
ahmadsidikrofi <rofidragon71@gmail.com>
2024-02-25 21:53:28 +0700

    jumbotron responsive-1

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ import PopulerFurniture from "../components/PopulerFurniture"
 import { Suspense } from "react"
 export default function Home() {
   return (
-    <section className='text-2xl font-medium pt-16 xl:px-16 sm:px-4 max-sm:px-4'>
-      <Jumbotron />
+    <>
+    <Jumbotron />
+    <section className='xl:px-16 sm:px-4 max-sm:px-4'>
       <FtCategories />
       <PopulerFurniture />
     </section>
+    </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,20 +42,20 @@ const Navbar = () => {
                 <SearchFurnitems setIsSearchOpen={setIsSearchOpen}/> : null
             }
             <div className="sticky top-0 bg-color-primary z-10 flex items-center py-3 px-4 gap-10 shadow-md">
-                <button onClick={miniMenuNavbar} className="p-[6px] visible lg:hidden md:hidden xl:hidden hover:bg-color-accent hover:bg-opacity-10 hover:rounded-full transition-all ease-in-out">
+                <button onClick={miniMenuNavbar} className="p-[6px] visible lg:hidden xl:hidden hover:bg-color-accent hover:bg-opacity-10 hover:rounded-full transition-all ease-in-out">
                     <List size={24} />
                 </button>
-                <Link href={'/'} className="text-md font-bold hidden md:block">FurniCraft</Link>
-                <div className="hidden md:flex md:items-center">
+                <Link href={'/'} className="text-md font-bold hidden lg:block xl:block">FurniCraft</Link>
+                <div className="hidden lg:flex lg:items-center xl:flex xl:items-center">
                     <button className="text-sm font-medium py-2 px-5 hover:bg-color-thin rounded-[10px] transition-all ease-linear">Lobby</button>
                     <button className="text-sm font-medium py-2 px-5 hover:bg-color-thin rounded-[10px] transition-all ease-linear">Categories</button>
                 </div>
 
                 <div className="flex gap-3 ml-auto">
-                    <button onClick={handleOpenSearch} className="visible lg:hidden md:hidden py-1 px-3 border-[1px] shadow border-color-accent border-opacity-15 rounded-full">
+                    <button onClick={handleOpenSearch} className="visible lg:hidden py-1 px-3 border-[1px] shadow border-color-accent border-opacity-15 rounded-full">
                         <MagnifyingGlass size={20}  className="font-medium"/>
                     </button>
-                    <div onClick={handleOpenSearch} className="cursor-pointer relative hidden md:flex md:items-center py-2 px-4 border-[1px] shadow border-color-accent border-opacity-15 hover:bg-color-grey hover:bg-opacity-5 rounded-full transition-all ease-linear 2s">
+                    <div onClick={handleOpenSearch} className="cursor-pointer relative hidden xl:flex xl:items-center lg:flex lg:items-center py-2 px-4 border-[1px] shadow border-color-accent border-opacity-15 hover:bg-color-grey hover:bg-opacity-5 rounded-full transition-all ease-linear 2s">
                         <MagnifyingGlass size={17}  className="absolute left-2 font-medium"/>
                         <p className="font-medium pl-4 text-sm">Cari Furnitems... <span className="rounded-full bg-sky-100 text-[10px] py-1 px-2 font-bold">Ctrl K</span> </p>
                     </div>
