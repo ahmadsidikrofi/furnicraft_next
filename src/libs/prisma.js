af180f2373b83d296d89dbc5e8a64644595cd605
ahmadsidikrofi <rofidragon71@gmail.com>
2024-08-02 20:56:33 +0700

    only first init on prisma

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,13 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-let prisma
-if (process.env.NODE_ENV === "production" ) {
-    prisma = new PrismaClient()
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
 } else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    } else {
-        prisma = global.prisma
-    }
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
-export default prisma
+
+export default prisma;
