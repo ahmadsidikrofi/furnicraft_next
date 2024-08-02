9793ff96cc93b593f25c21341c27e19753c59d1a
ahmadsidikrofi <rofidragon71@gmail.com>
2024-08-02 21:13:55 +0700

    change the prisma.js

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,14 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require("@prisma/client");
 
-let prisma;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 export default prisma;
