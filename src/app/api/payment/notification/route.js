ac0af6302842bd9ee9c10bc36e66601d42603821
ahmadsidikrofi <rofidragon71@gmail.com>
2024-08-09 14:23:38 +0700

    test success payment status-3

--- src/app/api/payment/notification/route.js
+++ src/app/api/payment/notification/route.js
@@ -1,18 +1,13 @@
 import prisma from "@/libs/prisma";
 import Midtrans from "midtrans-client";
 
-// Proceed with Midtrans verification and order status update
-let snap = new Midtrans.Snap({
-    isProduction: false,
-    serverKey: process.env.MIDTRANS_SECRET,
-    clientKey: process.env.MIDTRANS_PUBLIC_CLIENT
-})
-
 export async function POST(request) {
-    const { order_id, transaction_status } = await request.json()
-    console.log('Midtrans Notification Received:', { order_id, transaction_status })
+    const body = await request.json()
+    let orderId = body.order_id
+    let transaction_status = body.transaction_status
+    console.log('Midtrans Notification Received:', { orderId, transaction_status })
     // Handle missing properties
-    if (!order_id || !transaction_status) {
+    if (!body.order_id || !body.transaction_status) {
         return new Response(JSON.stringify({ status: 400, message: "Bad Request: Missing required fields" }), { status: 400 });
     }
 
@@ -20,7 +15,7 @@ export async function POST(request) {
         // Update status order menjadi 'SETTLEMENT' di database
         await prisma.orders.update({
             where: {
-                id: order_id
+                id: body.order_id
             },
             data: {
                 status: 'SETTLEMENT'
