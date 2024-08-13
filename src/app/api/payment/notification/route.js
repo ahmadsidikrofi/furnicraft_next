0d2fdbe00e31cca01a7ae96d4c4ff96e2bf6d61f
ahmadsidikrofi <rofidragon71@gmail.com>
2024-08-13 18:48:42 +0700

    fix payment on orders

--- src/app/api/payment/notification/route.js
+++ src/app/api/payment/notification/route.js
@@ -5,36 +5,23 @@ export async function POST(request) {
     const body = await request.json()
     let orderId = body.order_id
     let transaction_status = body.transaction_status
-    let fraud_status = body.fraud_status
     console.log('Midtrans Notification Received:', { orderId, transaction_status })
     // Handle missing properties
     if (!body.order_id || !body.transaction_status) {
         return new Response(JSON.stringify({ status: 400, message: "Bad Request: Missing required fields" }), { status: 400 });
     }
 
-    if (transaction_status === 'capture') {
-        if (transaction_status === 'settlement') {
-            // Update status order menjadi 'SETTLEMENT' di database
-            await prisma.orders.update({
-                where: {
-                    id: body.order_id
-                },
-                data: {
-                    status: 'SETTLEMENT'
-                }
-            })
-            return new Response(JSON.stringify({ message: 'Order status updated to SETTLEMENT' }), { status: 200 })
-        }  else if (fraud_status === 'accept') {
-            await prisma.orders.update({
-                where: {
-                    id: body.order_id
-                },
-                data: {
-                    status: 'SETTLEMENT'
-                }
-            })
-            return new Response(JSON.stringify({ message: 'Order status updated to SETTLEMENT' }), { status: 200 })
-        }
+    if (transaction_status === 'settlement') {
+        // Update status order menjadi 'SETTLEMENT' di database
+        await prisma.orders.update({
+            where: {
+                id: body.order_id
+            },
+            data: {
+                status: 'SETTLEMENT'
+            }
+        })
+        return new Response(JSON.stringify({ message: 'Order status updated to SETTLEMENT' }), { status: 200 })
     }
     return new Response(JSON.stringify({ message: 'Transaction status not handled' }), { status: 200 });
 }
