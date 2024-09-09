155332cce9b8b2121b24a0a7094c8ad012646d06
ahmadsidikrofi <rofidragon71@gmail.com>
2024-09-09 14:49:32 +0700

    update color primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,8 @@ module.exports = withUt({
       colors: {
         color: {
           primary: '#fff',
-          accent: '#FB6D48',
-          accent2: '#FB6D48',
+          accent: '#FFA823',
+          accent2: '#FFA823',
           thin: '#CCFFFF',
           secondary: '#393e46',
           grey: '#82748b'
